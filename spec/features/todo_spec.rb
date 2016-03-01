5220645bc4567506997a8295119ce2f4c8cf6806
Jay Hayes <ur@iamvery.com>
2016-02-29 20:13:21 -0500

    Add empty, skipped test for tasks index

---
+++ spec/features/todo_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+RSpec.describe "the todo app" do
+  describe "tasks index" do
+    it "displays all tasks" do
+      skip
+      # create a couple tasks
+      # visit the index page
+      # assert tasks are displayed
+    end
+  end
+end
